0ea4ea166f0c11e1eff72ad6aee403f012caf481
Joan Henao <henaobermeosteven@gmail.com>
2024-12-14 11:42:57 -0500

    Creacion Clase Product

---
+++ MyLibrary/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+	public class Product(int id, string name, decimal unitPrice, int unitsInStock)
+	
+	{
+		// Propiedades
+		public int Id { get; }
+		public string Name { get; }
+		public decimal UnitPrice { get; }
+		public int UnitInStock { get; }
+
+		//Constructor
+
+		
+	}
+	
+	
+}
